422358baa7d7950ecbf4d7798a13de8ba17a3268
Brian Van Nostrand <vannosb@wsdot.wa.gov>
2021-06-15 13:07:43 -0700

    Update Develop

---
+++ src/store.js
@@ -0,0 +1,55 @@
+define(["require", "exports", "vuex", "./esri-stuff/esriMap", "./layers/Basemaps"], function (require, exports, vuex_1, esriMap_1, Basemaps_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.useStore = exports.store = exports.key = void 0;
+    // define injection key...
+    exports.key = Symbol();
+    exports.store = vuex_1.createStore({
+        state: function () {
+            return {
+                basemap: "wsdot",
+                pointerX: 0,
+                pointerY: 0,
+                layerList: []
+            };
+        },
+        getters: {
+            completeLayerList: function (state) {
+                return state.layerList;
+            }
+        },
+        mutations: {
+            toggleBasemap: function (state) {
+                if (state.basemap == "wsdot") {
+                    state.basemap = "satellite";
+                }
+                else {
+                    state.basemap = "wsdot";
+                }
+                if (state.basemap == "wsdot") {
+                    esriMap_1.webmap.basemap = Basemaps_1.wsdotBasemap;
+                }
+                else {
+                    esriMap_1.webmap.basemap = Basemaps_1.satelliteBasemap;
+                }
+                console.log(state.basemap);
+                console.log(esriMap_1.webmap.basemap);
+            },
+            setPointerX: function (state, payload) {
+                state.pointerX = payload;
+            },
+            setPointerY: function (state, payload) {
+                state.pointerY = payload;
+            },
+            setLayerList: function (state, payload) {
+                state.layerList = payload;
+            }
+        },
+    });
+    // define custom useStore that supply key so do not have to do this in each component...
+    function useStore() {
+        return vuex_1.useStore(exports.key);
+    }
+    exports.useStore = useStore;
+});
+//# sourceMappingURL=store.js.map
