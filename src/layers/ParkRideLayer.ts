452929c767d7611a18ddf388b86f356325c892ce
Eric Jackson <jacksoe@wsdot.wa.gov>
2021-06-22 07:45:00 -0700

    change geojson extension to json

--- src/layers/ParkRideLayer.ts
+++ src/layers/ParkRideLayer.ts
@@ -10,7 +10,7 @@ const renderer = new SimpleRenderer({ symbol: Symbol });
 
 const layer = new GeoJSONLayer({
     id: "park-ride-layer",
-    url: "http://hqtob1webtmdev1/GISData/park-ride.geojson",
+    url: "http://hqtob1webtmdev1/GISData/park-ride.json",
     title: "Park and Rides",
     renderer: renderer,
     popupTemplate: Popup,
--- src/layers/PointRestrictionsLayer.ts
+++ src/layers/PointRestrictionsLayer.ts
@@ -16,7 +16,7 @@ const pointRestrictionsRenderer = new UniqueValueRenderer({
 })
 const PointRestrictionsLayer = new GeoJSONLayer({
     id: "point-restrictions-layer",
-    url: "http://hqtob1webtmdev1/GISData/park-ride.geojson",
+    url: "http://hqtob1webtmdev1/GISData/park-ride.json", //extension needs to be .json.  Server not configure to support .geojson.
     title: "Restriction Points",
     renderer: pointRestrictionsRenderer,
    // popupTemplate: Popup,
