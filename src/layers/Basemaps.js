b4a61dfa7929aad35abbb8e5aa5b165fa3a058e8
Masao Matsuoka <matsum@wsdot.wa.gov>
2021-08-05 10:06:21 -0700

    tsc

--- src/layers/Basemaps.js
+++ src/layers/Basemaps.js
@@ -3,7 +3,8 @@ import TileLayer from "@arcgis/core/layers/TileLayer";
 export const getDefaultBasemapInfo = () => { return basemaps[0]; };
 const urlBasemap = 
 //"https://data.wsdot.wa.gov/arcgis/rest/services/Shared/WebBaseMapWebMercator/MapServer";
-"https://tiles.arcgis.com/tiles/IYrj3otxNjPsrTRD/arcgis/rest/services/TravelCenterBasemapTileTest/MapServer";
+//"https://tiles.arcgis.com/tiles/IYrj3otxNjPsrTRD/arcgis/rest/services/TravelCenterBasemapTileTest/MapServer"
+"https://tiles.arcgis.com/tiles/IYrj3otxNjPsrTRD/arcgis/rest/services/Travel_Center_Basemap_Dev/MapServer";
 // Array of basemaps. The first one is the default.
 const basemaps = [
     {
