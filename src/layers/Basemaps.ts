474a615f7f19370389b06e04bcb69216f9cd6705
Masao Matsuoka <matsum@wsdot.wa.gov>
2021-08-05 10:03:34 -0700

    New basemap, disable auto zoom on click

--- src/layers/Basemaps.ts
+++ src/layers/Basemaps.ts
@@ -4,7 +4,8 @@ import BasemapInfo from "@/types/BasemapInfo";
 export const getDefaultBasemapInfo = (): BasemapInfo => { return basemaps[0] }
 const urlBasemap =
     //"https://data.wsdot.wa.gov/arcgis/rest/services/Shared/WebBaseMapWebMercator/MapServer";
-    "https://tiles.arcgis.com/tiles/IYrj3otxNjPsrTRD/arcgis/rest/services/TravelCenterBasemapTileTest/MapServer"
+    //"https://tiles.arcgis.com/tiles/IYrj3otxNjPsrTRD/arcgis/rest/services/TravelCenterBasemapTileTest/MapServer"
+    "https://tiles.arcgis.com/tiles/IYrj3otxNjPsrTRD/arcgis/rest/services/Travel_Center_Basemap_Dev/MapServer"
 
 // Array of basemaps. The first one is the default.
 const basemaps: BasemapInfo[] = [
