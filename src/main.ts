0cb5de5ed749f226edb58fc7279246152c679b69
Masao Matsuoka <matsum@wsdot.wa.gov>
2021-08-12 08:12:34 -0700

    comment

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ import "./assets/flexGridView.css"
 import { store, key } from "./store";
 import { getConfig } from "./utils/appConfigUtil";
 
+// Load config before app starts...
 getConfig().then(() => {
-
     // adding store as a plugin while creating an app...
     createApp(App).use(store, key).mount('#app');
 });
