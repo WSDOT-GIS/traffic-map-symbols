fc8310841d8d0a89025a8248ca0d3b52b20d1ceb
Brian Van Nostrand <vannosb@wsdot.wa.gov>
2021-08-12 09:59:41 -0700

    pulling Masao's changes

--- src/utils/appConfigUtil.js
+++ src/utils/appConfigUtil.js
@@ -1,12 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getConfig = void 0;
+const tslib_1 = require("tslib");
 let appConfig;
-export const getConfig = async () => {
+const getConfig = () => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
     if (!appConfig) {
         console.log("************ Fetch config...");
-        const fetchResponse = await fetch("/appconfig.json");
-        const config = await fetchResponse.json();
+        const fetchResponse = yield fetch("/appconfig.json");
+        const config = yield fetchResponse.json();
         appConfig = config;
     }
     console.log("************ Got config...");
     return appConfig;
-};
+});
+exports.getConfig = getConfig;
 //# sourceMappingURL=appConfigUtil.js.map
