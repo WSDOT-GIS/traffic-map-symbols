01c3a4d7fe6757ad9c82eb587ed082f846a2436b
Eric Jackson <jacksoe@wsdot.wa.gov>
2021-08-19 16:58:21 -0700

    Include url path when reading appconfig

--- src/utils/appConfigUtil.ts
+++ src/utils/appConfigUtil.ts
@@ -5,7 +5,10 @@ let appConfig: AppConfig | undefined;
 
 export const getConfig = async (): Promise<AppConfig> => {
     if (!appConfig) {
-        const fetchResponse = await fetch("/appconfig.json");
+        //include path where application was loaded from
+        var href = window.location.pathname;
+        var dir = href.substring(0, href.lastIndexOf('/'));
+        const fetchResponse = await fetch(dir + "/appconfig.json");
         const config = await fetchResponse.json();
         appConfig = config as AppConfig;
     }
