a8880a3adb3b2e922944eb28f906a47f24117b5f
Eric Jackson <jacksoe@wsdot.wa.gov>
2021-08-19 17:11:09 -0700

    updates to appconfigutil

--- src/utils/appConfigUtil.ts
+++ src/utils/appConfigUtil.ts
@@ -5,9 +5,9 @@ let appConfig: AppConfig | undefined;
 
 export const getConfig = async (): Promise<AppConfig> => {
     if (!appConfig) {
-        //include path where application was loaded from
-        var href = window.location.pathname;
-        var dir = href.substring(0, href.lastIndexOf('/'));
+        //include path where application was loaded from.  QA/Prod will be different than local dev.
+        const href = window.location.pathname;
+        const dir = href.substring(0, href.lastIndexOf('/'));
         const fetchResponse = await fetch(dir + "/appconfig.json");
         const config = await fetchResponse.json();
         appConfig = config as AppConfig;
