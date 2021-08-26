d0c8dcfc61abb72d21e4323b95247c2e9c28f01f
Jeff Jacobson <jacobsj@wsdot.wa.gov>
2021-08-26 15:18:58 -0700

    Removed built JavaScript and mapping files
    added removed files to .gitignore

--- src/types/FeatureInfo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAttribute = void 0;
-const getAttribute = (info, fieldName) => {
-    const x = info.attributes[fieldName];
-};
-exports.getAttribute = getAttribute;
-//# sourceMappingURL=FeatureInfo.js.map
