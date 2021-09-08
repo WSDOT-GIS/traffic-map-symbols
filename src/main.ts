c668317ad76dbf5ccb633059fa562a4f50a89740
Masao Matsuoka <matsum@wsdot.wa.gov>
2021-09-08 08:21:40 -0700

    Removed toggleSlider.css

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import "./assets/global.css"
 import "./assets/w3.css"
 import "./assets/main.css"
-import "./assets/toggleSlider.css"
 import "./assets/flexGridView.css"
 import { store, key } from "./store";
 import { getConfig } from "./utils/appConfigUtil";
