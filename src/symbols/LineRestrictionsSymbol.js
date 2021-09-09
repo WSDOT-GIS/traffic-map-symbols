4ec5cc6c1e7f52685d602806983699739ccd783f
Brian Van Nostrand <vannosb@wsdot.wa.gov>
2021-09-08 18:23:54 -0700

    updated line symbol

--- src/symbols/LineRestrictionsSymbol.js
+++ src/symbols/LineRestrictionsSymbol.js
@@ -5,14 +5,14 @@ const tslib_1 = require("tslib");
 const SimpleLineSymbol_1 = tslib_1.__importDefault(require("@arcgis/core/symbols/SimpleLineSymbol"));
 const roadRestrictionLine = new SimpleLineSymbol_1.default({
     style: "solid",
-    color: [204, 32, 156, 0.3],
+    color: [204, 32, 156, 0.7],
     width: "15px",
     cap: "square"
 });
 exports.roadRestrictionLine = roadRestrictionLine;
 const bridgeRestrictionLine = new SimpleLineSymbol_1.default({
     style: "solid",
-    color: [204, 32, 156, 0.3],
+    color: [204, 32, 156, 0.7],
     width: "15px",
 });
 exports.bridgeRestrictionLine = bridgeRestrictionLine;
