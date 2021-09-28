b668360b73104ecc7f3264477a1a991a2027e544
Brian Van Nostrand <vannosb@wsdot.wa.gov>
2021-09-28 14:23:52 -0700

    Fixed casing in layer list titles

--- src/components/LayerListView.vue
+++ src/components/LayerListView.vue
@@ -15,7 +15,7 @@
         >
           <template v-slot>
             <!-- <div class="mapFeaturesIcon"></div> -->
-            <span>Traffic Flow</span>
+            <span>Traffic flow</span>
           </template>
         </ToggleSwitchView>
       </li>
@@ -188,7 +188,7 @@
               "
             ></div>
             <span class="listLabel" id="CommercialVehicleLabel"
-              >Truck Restrictions</span
+              >Truck restrictions</span
             >
           </template>
         </ToggleSwitchView>
@@ -212,9 +212,7 @@
               "
             ></div>
             <span class="listLabel">
-              {{
-                layerList[getLayerIndex("mountain-passes-layer")].title
-              }}</span
+              Mountain pass reports</span
             >
           </template>
         </ToggleSwitchView>
@@ -238,9 +236,7 @@
               "
             ></div>
             <span class="listLabel">
-              {{
-                layerList[getLayerIndex("weather-stations-layer")].title
-              }}</span
+              Weather stations</span
             >
           </template>
         </ToggleSwitchView>
@@ -262,7 +258,7 @@
               v-html="layerIcons.find((x) => x.id == 'rest-areas-layer')?.paths"
             ></div>
             <span class="listLabel">
-              {{ layerList[getLayerIndex("rest-areas-layer")].title }}</span
+              Rest areas</span
             >
           </template>
         </ToggleSwitchView>
@@ -282,7 +278,7 @@
               v-html="layerIcons.find((x) => x.id == 'park-ride-layer')?.paths"
             ></div>
             <span class="listLabel">
-              {{ layerList[getLayerIndex("park-ride-layer")].title }}</span
+              {{`Park & Rides`}}</span
             >
           </template>
         </ToggleSwitchView>
@@ -306,7 +302,7 @@
               "
             ></div>
             <span class="listLabel">
-              {{ layerList[getLayerIndex("travel-times-layer")].title }}</span
+              Travel times</span
             >
           </template>
         </ToggleSwitchView>
@@ -331,7 +327,7 @@
               "
             ></div>
             <span class="listLabel" id="CommercialVehicleLabel"
-              >Border Crossings</span
+              >Border crossings</span
             >
           </template>
         </ToggleSwitchView>
@@ -357,7 +353,7 @@
               "
             ></div>
             <span class="listLabel" id="WildlandFireeLabel"
-              >Wildland Fires</span
+              >Wildland fires</span
             >
           </template>
         </ToggleSwitchView>
