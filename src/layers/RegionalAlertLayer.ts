23578d0d30881ccfc69260305fcce8108b28b35c
Masao Matsuoka <matsum@wsdot.wa.gov>
2021-09-28 10:44:38 -0700

    Fixed AlertAreaLayer Name field issue

--- src/layers/RegionalAlertLayer.ts
+++ src/layers/RegionalAlertLayer.ts
@@ -100,17 +100,17 @@ export const initLayer = async (alertUrl: string, countyUrl: string, regionUrl: 
     for (const each of json.features) {
         // Get region boundary from county or region service...
         let where = "";
-        let outFields = "";
+        let nameField = "";
         let url = "";
         switch (each.attributes.EventCategoryType) {
             case "County":
                 where = "JURDSG";
-                outFields = "JURLBL";
+                nameField = "JURLBL";
                 url = countyUrl;
                 break;
             case "Region":
                 where = "DistrictNumber";
-                outFields = "RegionName";
+                nameField = "RegionName";
                 url = regionUrl;
                 break;
             default:
@@ -119,7 +119,7 @@ export const initLayer = async (alertUrl: string, countyUrl: string, regionUrl: 
         }
         where += `=${each.attributes.CountyID}`;
         response = await fetch(
-            `${url}/query?where=${encodeURIComponent(where)}&outFields=${outFields}&returnGeometry=true&outSR=3857&f=pjson`);
+            `${url}/query?where=${encodeURIComponent(where)}&outFields=${nameField}&returnGeometry=true&outSR=3857&f=pjson`);
         json = await response.json();
         // Get centroid and set that as alert's geometry
         if (!json.features || json.features.length === 0) {
@@ -128,7 +128,8 @@ export const initLayer = async (alertUrl: string, countyUrl: string, regionUrl: 
             continue;
         }
         else {
-            const areaGeom = Polygon.fromJSON(json.features[0].geometry);
+            const areaFeature = json.features[0];
+            const areaGeom = Polygon.fromJSON(areaFeature.geometry);
             areaGeom.spatialReference = SpatialReference.fromJSON(json.spatialReference);
             // Add an alert feature...
             //each.attributes.ExtendedMessage = "Sit laborum qui sunt nostrud nulla sint laboris ullamco dolore fugiat aute adipisicing ad cupidatat. Deserunt non velit adipisicing in duis et exercitation esse amet consequat pariatur. Qui nulla commodo labore pariatur dolore enim ipsum aute nulla nisi ullamco fugiat et. Lorem ut ea dolore commodo esse quis sunt incididunt. Cillum irure velit occaecat est cupidatat nisi in pariatur sint. Sunt laboris officia ad qui enim do Lorem. Consequat sint aliquip incididunt dolor et nulla consequat aute sint.";
@@ -141,7 +142,7 @@ export const initLayer = async (alertUrl: string, countyUrl: string, regionUrl: 
                 geometry: areaGeom,
                 attributes: {
                     EventID: each.attributes.EventID,
-                    Name: each.attributes[outFields],
+                    Name: areaFeature.attributes[nameField],
                 }
             }));
         }
