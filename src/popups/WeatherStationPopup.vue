8e40eaafe6e94f37cd0e48bc3ecc2dbf4e2a45d7
Brian Van Nostrand <vannosb@wsdot.wa.gov>
2021-10-18 13:35:42 -0700

    Added sorting to forecast data in weather station pop ups

--- src/popups/WeatherStationPopup.vue
+++ src/popups/WeatherStationPopup.vue
@@ -121,13 +121,18 @@ export default defineComponent({
             //fetch(config.forecastSummaryAPI+featureNWSZoneId).then((result)=>{ ~~summary call
             fetch(config.forecastExtendedAPI + featureNWSZoneId).then((result) => {
               result.json().then((response) => {
-                //console.log(response.forecastData)
+                console.log(response.forecastData)
+                function mycomparator(a:any,b:any) {
+                  return parseInt(a.forecastNumber, 10) - parseInt(b.forecastNumber, 10);
+                }
+                const sortedForecasts = response.forecastData.sort(mycomparator);
                 forecastList.value = {
                   nwsZoneId: response.nwsZoneId,
                   forecastDateTime: response.forecastDateTime,
                   forecastExpirationDateTime: response.forecastExpirationDateTime,
                   nwsZoneRegionName: response.nwsZoneRegionName,
-                  forecasts: response.forecastData,
+                  forecasts: sortedForecasts,
+                  //forecasts: response.forecastData,
                 };
                 feature.value = featureresult;
               });
