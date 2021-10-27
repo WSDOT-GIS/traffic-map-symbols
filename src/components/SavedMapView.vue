7e416194e77bd78d1196735ce79a171c9b628323
Masao Matsuoka <matsum@wsdot.wa.gov>
2021-10-27 13:54:48 -0700

    Made the delete saved map button to 24px

--- src/components/SavedMapView.vue
+++ src/components/SavedMapView.vue
@@ -260,13 +260,13 @@ a {
   background-color: var(--color-gray20);
   border: none;
   color: var(--color-gray100);
-  font-size: var(--type-scale-base0);
+  font-size: var(--type-scale-base7);
   font-weight: var(--font-weight-normal);
-  line-height: var(--type-scale-base-2);
+  line-height: var(--type-scale-base-6);
   text-align: center;
   border-radius: 50%;
-  width: var(--type-scale-base0);
-  height: var(--type-scale-base0);
+  width: 24px;
+  height: 24px;
   padding: 0.1rem;
 }
 </style>
