c3306c15239e6c16ca0981437ea9c818f7ba4d56
Masao Matsuoka <matsum@wsdot.wa.gov>
2021-10-27 13:28:20 -0700

    in progress

--- src/popups/PopupBase.vue
+++ src/popups/PopupBase.vue
@@ -319,7 +319,7 @@ export default defineComponent({
       };
     });
 
-    const onClickAway = (event: PointerEvent) => {
+    const onClickAway = (event: PointerEvent | TouchEvent) => {
       console.log(event);
       if (smallMedia.value) {
         close();
@@ -328,6 +328,7 @@ export default defineComponent({
            - If user clicks on something other than the map (e.g. TOC, header, ...), then close the popup.
            - If user clicks on map, then do not do anything here. */
         const target = event.target as HTMLElement;
+        console.log(target.classList);
         if (!target.classList.contains("esri-view-surface")) {
           close();
         }
