186ed61c1f7f4aea413cc4144022ae3d7909ab4a
Masao Matsuoka <matsum@wsdot.wa.gov>
2021-11-01 17:35:12 -0700

    TASK 42409

--- src/components/AdView.vue
+++ src/components/AdView.vue
@@ -1,19 +1,18 @@
 <template>
-  <div id="div-gpt-ad-1632317155034-0" class="w3-content ad-container" ref="containerDiv">
-    <button
-      title="Close advertisement"
-      aria-label="Close advertisement"
-      class="ad-close-btn w3-button"
-      @click="close"
-    >
-      &times;
-    </button>
+  <div id="div-gpt-ad-1632317155034-0" class="ad-container" ref="containerDiv"></div>
+  <div
+    title="Close advertisement"
+    aria-label="Close advertisement"
+    id="map-ad-close-btn"
+    class="w3-button"
+    @click="close"
+  >
+    &times;
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
-import {} from "doubleclick-gpt";
 
 export default defineComponent({
   emits: ["onResize"],
@@ -25,12 +24,10 @@ export default defineComponent({
   },
   setup(props, context) {
     const containerDiv = ref<HTMLDivElement>();
-    // GPT slots
-    const gptAdSlots: Slot[] = [];
     //
     window.googletag = window.googletag || { cmd: [] };
     const googletag = window.googletag;
-    
+
     onMounted(() => {
       // To make this code working, make sure to setup the followings...
       //  1. Import the gpt.js in the index.html
@@ -39,7 +36,7 @@ export default defineComponent({
       //  3. Declare the window extension in the globals.d.ts
       //window.googletag = window.googletag || { cmd: [] };
       // GPT slots
-      //const gptAdSlots = [];
+      const gptAdSlots = [];
       // const googletag = window.googletag;
       /* Use googletag.cmd to queue callbacks for when GPT is ready. 
         These callbacks do not have to check googletag.apiReady as they are guaranteed to execute once the API is set up.*/
@@ -109,7 +106,9 @@ export default defineComponent({
       }
     };
     const close = () => {
-      window.googletag.destroySlots(gptAdSlots);
+      googletag.destroySlots();
+      document.getElementById("map-ad-close-btn")?.remove();
+      context.emit("onResize", { width: 0, height: 0 });
     };
     return { containerDiv, onResize, close };
   },
@@ -123,23 +122,31 @@ export default defineComponent({
   min-height: 50px;
   min-width: 300px;
 }
-#ad-close-btn {
+#map-ad-close-btn {
+  position: absolute;
+  top: 0;
+  right: 0;
   background-color: #000;
   border: none;
-  color: #fff;
+  color: var(--color-gray20);
   font-size: var(--type-scale-base2);
   font-weight: var(--font-weight-normal);
-  line-height: var(--type-scale-base-2);
+  line-height: var(--type-scale-base2);
   text-align: center;
   border-radius: 50%;
-  width: 1rem;
-  height: 1rem;
+  width: 1.5rem;
+  height: 1.5rem;
   padding: 0.1rem;
 }
 @media screen and (min-width: 730px) {
   .ad_container {
     min-height: 90px;
     min-width: 728px;
+  }
+  #map-ad-close-btn {
+    position: absolute;
+    top: -0.2rem;
+    right: -0.3rem;
   }
 }
 p {
