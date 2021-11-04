92d2d826ff90cd7f5a925570d835ce31e04a7ad1
Masao Matsuoka <matsum@wsdot.wa.gov>
2021-11-04 10:47:28 -0700

    BUG 42537

--- src/components/AlertView.vue
+++ src/components/AlertView.vue
@@ -4,6 +4,7 @@
       class="w3-modal-content w3-animate-right w3-card w3-left-align alert-content"
       :style="{ maxHeight: height }"
       ref="containerRef"
+      v-click-away="onClickAway"
     >
       <div class="alert-header">
         <div class="alert-banner">
@@ -44,8 +45,9 @@
     id="alert-container-closed"
     class="w3-transparent w3-button w3-circle"
     @click="toggleDisplay"
+    
   >
-    <div v-html="iconButton?.paths" class="alert-button"></div>
+    <div v-html="iconButton?.paths" class="alert-button" ref="btnContainerRef"></div>
   </div>
 </template>
 
@@ -67,6 +69,7 @@ export default defineComponent({
   setup(props) {
     const store = useStore();
     const containerRef = ref<HTMLDivElement>();
+    const btnContainerRef = ref<HTMLDivElement>();
     const mapSize = computed(() => store.state.mapSize);
     const isOpen = ref(true);
     const sortedAlerts = ref<AlertInfo[]>([]);
@@ -103,16 +106,32 @@ export default defineComponent({
       const h = mapSize.value.height - top * 2;
       height.value = h + "px";
     };
+    let justOpened: boolean | undefined;
     const toggleDisplay = () => {
       isOpen.value = !isOpen.value;
       setDisplayStyle();
+      if (isOpen.value) {
+        justOpened = true;
+      }
     };
     const setDisplayStyle = () => {
       displayStyle.value =
         isOpen.value && props.Alerts.length > 0 && props.Alerts[0] ? "block" : "none";
+        console.log("setDisplayStyle: " + btnContainerRef.value)
+    };
+    const onClickAway = () => {
+      // If the button is clicked, the window will be opened, then onClickAway is called, so it will close it again.
+      // To prevent that, the justOpened flag is set to true when that happens.
+      if (justOpened) {
+        justOpened = false;
+        return;
+      }
+      isOpen.value = false;
+      setDisplayStyle();
     };
     return {
       containerRef,
+      btnContainerRef,
       height,
       isOpen,
       sortedAlerts,
@@ -121,6 +140,7 @@ export default defineComponent({
       formatEpoch,
       iconBanner,
       iconButton,
+      onClickAway,
     };
   },
 });
