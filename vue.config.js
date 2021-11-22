c9634b49ae8597cfa3b5ff9e19129054bd416b50
Masao Matsuoka <matsum@wsdot.wa.gov>
2021-11-22 09:40:40 -0800

    Added publicPath to vue.config

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
     // devServer: {
     //     disableHostCheck: true
     // },
+    publicPath: process.env.NODE_ENV === 'production' ? '/Travel/Real-time/Map/' : '/',
     configureWebpack: {
         devtool: "source-map",
         plugins: [
