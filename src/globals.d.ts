2b4668c70e15794f4f0eac448dc194791eb70d82
Brian Van Nostrand <vannosb@wsdot.wa.gov>
2021-11-23 17:05:40 -0800

    Added ApplicationInsight code and dependency

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,30 +1,30 @@
-declare var map: any;
-//declare var sketchViewModel: any
-declare var boundaryDrawState: string
-declare var extendedWeekendURL: string
-declare var continuousWeekendURL: string
-declare var offPeakDaytimeURL: string
-declare var nighttimeOnlyURL: string
-declare var minorImpactURL: string
-declare var missingInformationURL: string
-declare var noTrafficImpactURL: string
-//Ignore declare var boundarySketchLayer: any
-declare var WINs: string[];
-declare var PINs: string[];
-declare var trafficwaySegmentData: any[]
+declare const map: any;
+//declare const sketchViewModel: any
+declare const boundaryDrawState: string
+declare const extendedWeekendURL: string
+declare const continuousWeekendURL: string
+declare const offPeakDaytimeURL: string
+declare const nighttimeOnlyURL: string
+declare const minorImpactURL: string
+declare const missingInformationURL: string
+declare const noTrafficImpactURL: string
+//Ignore declare const boundarySketchLayer: any
+declare const WINs: string[];
+declare const PINs: string[];
+declare const trafficwaySegmentData: any[]
 declare namespace JSX {
     interface IntrinsicElements {
         heyo: any
     }
 }
-declare var mapLayers: any[]
-declare var extendedWeekendLayer: any
-declare var continuousWeekendLayer: any
-declare var offPeakDaytimeLayer: any
-declare var nighttimeOnlyLayer: any
-declare var minorImpactLayer: any
-declare var missingInformationLayer: any
-declare var noTrafficImpactLayer: any
+declare const mapLayers: any[]
+declare const extendedWeekendLayer: any
+declare const continuousWeekendLayer: any
+declare const offPeakDaytimeLayer: any
+declare const nighttimeOnlyLayer: any
+declare const minorImpactLayer: any
+declare const missingInformationLayer: any
+declare const noTrafficImpactLayer: any
 // For the google ad...
 // Also installed via npm doubleclick-gpt for the type definition.
 interface Window {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import VueClickAway from "vue3-click-away";
+import {ApplicationInsights}from '@microsoft/applicationinsights-web'
 import "./assets/global.css"
 import "./assets/w3.css"
 import "./assets/main.css" // WATECH CSS
@@ -10,7 +11,20 @@ import { loadConfig } from "./utils/appConfigUtil";
 
 // Load config before app starts...
 loadConfig().then(() => {
+    const appInsights = new ApplicationInsights(
+    {
+        config:{
+            //instrumentationKey: '3f639dd2-45dd-4de8-afc2-3e6bc599e381',
+            enableAutoRouteTracking: true,
+            enableUnhandledPromiseRejectionTracking: true,
+            autoTrackPageVisitTime: true,
+            excludeRequestFromAutoTrackingPatterns: ['googlesyndication','doubleclick','google-analytics']
+        }
+    })
+    appInsights.loadAppInsights()
+    appInsights.trackPageView()
     // adding store as a plugin while creating an app...
-    createApp(App).use(store, key).use(VueClickAway).mount('#app');
-
+    const app = createApp(App)
+    app.use(store, key).use(VueClickAway).mount('#app');
+    app.provide('$appInsights', appInsights)
 });
