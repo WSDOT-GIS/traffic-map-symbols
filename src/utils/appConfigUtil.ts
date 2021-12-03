a898e99ee289df4dbce88da1074e509984053724
Brian Van Nostrand <vannosb@wsdot.wa.gov>
2021-12-02 18:39:21 -0800

    Updated state route shield layer

--- src/utils/appConfigUtil.ts
+++ src/utils/appConfigUtil.ts
@@ -7,7 +7,7 @@ export const loadConfig = async (): Promise<AppConfig> => {
         //include path where application was loaded from.  QA/Prod will be different than local dev.
         const href = window.location.pathname;
         const dir = href.substring(0, href.lastIndexOf('/'));
-        const fetchResponse = await fetch(dir + "/appconfigDev.json");
+        const fetchResponse = await fetch(dir + "/appConfig.json");
         const config = await fetchResponse.json();
         appConfig = config as AppConfig;
         console.log(appConfig)
