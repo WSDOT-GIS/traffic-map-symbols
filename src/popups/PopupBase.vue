1eca1cc2f85fe7c7e4f3fb6f7fa59233a13db4f2
Masao Matsuoka <matsum@wsdot.wa.gov>
2022-01-13 10:24:36 -0800

    Increased z-index for alert view

--- src/popups/PopupBase.vue
+++ src/popups/PopupBase.vue
@@ -356,7 +356,6 @@ export default defineComponent({
     //Assigns the weather forecast to the weather forecast ref
     const setWeatherForecast = () => {
       if (props.Config.weatherForecast) {
-        console.log(props.Config.weatherForecast)
         propWeatherForecast.value = props.Config.weatherForecast;
       }
     };
