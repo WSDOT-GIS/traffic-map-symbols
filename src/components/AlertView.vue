57f0dd84de2ea9881c79e44dd6167523d6a40353
Masao Matsuoka <matsum@wsdot.wa.gov>
2022-01-13 10:24:36 -0800

    Increased z-index for alert view

--- src/components/AlertView.vue
+++ src/components/AlertView.vue
@@ -150,7 +150,7 @@ export default defineComponent({
 <style scoped>
 #alert-container-open {
   /* This needs to be higher than the popup's z-index, so popup is displayed under the gray background. */
-  z-index: 10;
+  z-index: 99;
 }
 
 #alert-container-closed {
