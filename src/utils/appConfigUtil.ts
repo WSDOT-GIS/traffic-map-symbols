0fb2f12fc47fc83047beac16cb2e5e79134adabd
Masao Matsuoka <matsum@wsdot.wa.gov>
2022-01-20 16:37:44 -0800

    Fix appConfig URL

--- src/utils/appConfigUtil.ts
+++ src/utils/appConfigUtil.ts
@@ -5,9 +5,10 @@ let appConfig: AppConfig | undefined;
 export const loadConfig = async (): Promise<AppConfig> => {
     if (!appConfig) {
         //include path where application was loaded from.  QA/Prod will be different than local dev.
-        // const href = window.location.pathname;
-        // const dir = href.substring(0, href.lastIndexOf('/'));
-        const fetchResponse = await fetch("/appConfig.json");
+        const href = window.location.pathname;
+        let dir = href.toLowerCase().substring(0, href.lastIndexOf("/travel/real-time/map"));
+        dir = !dir ? "" : dir;
+        const fetchResponse = await fetch(dir + "/appConfig.json");
         const config = await fetchResponse.json();
         appConfig = config as AppConfig;
     }
