fbf58fdcc403df86ef97d44ed5754e3dcb089ce4
Brian Van Nostrand <vannosb@wsdot.wa.gov>
2022-01-31 14:35:32 -0800

    Added WebPageName to MountainPass layer and popup url

--- src/utils/urlParamUtil.ts
+++ src/utils/urlParamUtil.ts
@@ -110,6 +110,7 @@ export const setVisibleLayersFromUrl = (layerList: LayerInfo[], route: RouteLoca
  */
 export const getFeatureIdFromUrl = (route: RouteLocationNormalizedLoaded): string | null => {
     let id: string | null;
+    console.log(route.params);
     if (route.params.featureid) {
         const p = route.params.featureid;
         id = typeof p === 'string' ? p : p[0];
