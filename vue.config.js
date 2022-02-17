6f4cf707bde639a76411e9da4e65f3b26adeb4d3
Masao Matsuoka <matsum@wsdot.wa.gov>
2022-02-17 13:36:36 -0800

    Comment out devServer

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,9 @@ const zlib = require("zlib");
 
 module.exports = {
     // Uncomment below for testing Local Host on BrowserStack
-    devServer: {
-        disableHostCheck: true
-    },
+    // devServer: {
+    //     disableHostCheck: true
+    // },
     // publicPath for production must include path to the app, root works for dev
     publicPath: process.env.NODE_ENV === 'production' ? '/Travel/Real-time/Map/' : '/',
     configureWebpack: {
