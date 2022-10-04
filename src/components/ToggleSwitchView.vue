b9b0274ccf185ca1cbd9569aecc7ab2afb1ca8d8
Brian Van Nostrand <vannosb@wsdot.wa.gov>
2022-10-04 12:17:01 -0700

    Added GA populate 'click' event with toggle layer.

--- src/components/ToggleSwitchView.vue
+++ src/components/ToggleSwitchView.vue
@@ -19,7 +19,7 @@ import { defineComponent } from "vue";
 import {Analytics} from 'analytics';
 import googleAnalytics from '@analytics/google-analytics';
 import { getConfig } from "@/utils/appConfigUtil";
-import { GtagEvent } from "vue-gtag";
+import { event } from "vue-gtag";
 export default defineComponent({
   props: {
     Checked: {
@@ -41,8 +41,7 @@ export default defineComponent({
   },
   setup(props, context) {
     const config = getConfig();
-    let analytics:any;
-    let gtag:GtagEvent
+    /*let analytics:any;
     try{
       analytics = Analytics({
         app: 'WSDOT',
@@ -55,14 +54,16 @@ export default defineComponent({
     }
     catch(e){
       console.error(e)
-    }
+    }*/
     
     const onToggle = (evt: Event) => {
       const target = evt.currentTarget as HTMLInputElement;
       const sendToggleOn = ()=>{
        
         try{
-          window.gtag("event",'Toggle_Cameras_On')
+          console.log(window)
+          event('click',{toggle_map_layer_on: props.Title})
+          //window.gtag('event','toggle_on')
           /*analytics.track('toggle', {
           category: 'Layer',
           label: props.Title+"-"+"On",
@@ -76,7 +77,8 @@ export default defineComponent({
       }
       const sendToggleOff = ()=>{
         try{
-            window.gtag("event",`Toggle_Cameras_Off`)
+          console.log(window.gtag)
+          event('click',{'toggle_map_layer_off': props.Title})
             /*analytics.track('toggle', {
             category: 'Layer',
             label: props.Title+"-"+"Off",
