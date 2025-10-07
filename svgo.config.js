4a77418f7eaf9c188e51817aeeb1308e8056976c
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2025-10-07 14:53:52 -0700

    style: prettier

--- svgo.config.js
+++ svgo.config.js
@@ -5,26 +5,26 @@
  */
 
 module.exports = {
-    multipass: true, // boolean. false by default
-    // datauri: 'enc', // 'base64' (default), 'enc' or 'unenc'.
-    js2svg: {
-        // indent: 2, // string with spaces or number of spaces. 4 by default
-        pretty: true, // boolean, false by default
-    },
-    convertColors: false,
-    plugins: [
-        // set of built-in plugins enabled by default
-        'preset-default',
+	multipass: true, // boolean. false by default
+	// datauri: 'enc', // 'base64' (default), 'enc' or 'unenc'.
+	js2svg: {
+		// indent: 2, // string with spaces or number of spaces. 4 by default
+		pretty: true, // boolean, false by default
+	},
+	convertColors: false,
+	plugins: [
+		// set of built-in plugins enabled by default
+		"preset-default",
 
-        //   // enable built-in plugins by name
-        //   'prefixIds',
+		//   // enable built-in plugins by name
+		//   'prefixIds',
 
-        //   // or by expanded notation which allows to configure plugin
-        //   {
-        //     name: 'sortAttrs',
-        //     params: {
-        //       xmlnsOrder: 'alphabetical',
-        //     },
-        //   },
-    ],
+		//   // or by expanded notation which allows to configure plugin
+		//   {
+		//     name: 'sortAttrs',
+		//     params: {
+		//       xmlnsOrder: 'alphabetical',
+		//     },
+		//   },
+	],
 };
