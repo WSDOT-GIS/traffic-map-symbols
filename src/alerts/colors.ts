7f9fa66a172c0b92bcd1667ae4c5af3271504553
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2025-10-14 14:04:47 -0700

    refactor: add alertsColorRamp
    docs: :memo: add JSDoc comments

--- src/alerts/colors.ts
+++ src/alerts/colors.ts
@@ -17,6 +17,13 @@ export const low = [255, 193, 7];
 
 /**
  * A mapping of alert values to their integer equivalents.
+ *
+ * Travel Center Priority ID | Label
+ * -------------------------:|----------
+ *                         1 | Closure
+ *                         2 | High
+ *                         3 | Medium
+ *                         4 | Low
  */
 export const priorities = new Map([
 	[1, closure],
@@ -24,3 +31,12 @@ export const priorities = new Map([
 	[3, medium],
 	[4, low],
 ] as const);
+
+/**
+ * Color ramp for alerts, from {@link closure} (1)
+ * to {@link low} (4).
+ */
+export const alertsColorRamp: __esri.CIMColorRamp = {
+	type: "CIMFixedColorRamp",
+	colors: [closure, high, medium, low],
+};
